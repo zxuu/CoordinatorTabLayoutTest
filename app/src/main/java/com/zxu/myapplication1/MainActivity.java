120c5307085a9dc18cd09a66efbe50f5d93b2d8c
zxu <z18255713582@outlook.com>
2019-01-28 20:09:54 +0800

    1

---
+++ app/src/main/java/com/zxu/myapplication1/MainActivity.java
@@ -0,0 +1,58 @@
+package com.zxu.myapplication1;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import cn.hugeterry.coordinatortablayout.CoordinatorTabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private CoordinatorTabLayout mCoordinatorTabLayout;
+    private int[] mImageArray, mColorArray;
+    private ArrayList<Fragment> mFragments;
+    private final String[] mTitles = {"Android", "iOS", "Web", "Other"};
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initFragments();
+        initViewPager();
+        mImageArray = new int[]{
+                R.mipmap.bg_android,
+                R.mipmap.bg_ios,
+                R.mipmap.bg_js,
+                R.mipmap.bg_other};
+        mColorArray = new int[]{
+                android.R.color.holo_blue_light,
+                android.R.color.holo_red_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_green_light};
+
+        mCoordinatorTabLayout = (CoordinatorTabLayout) findViewById(R.id.coordinatortablayout);
+        mCoordinatorTabLayout.setTranslucentStatusBar(this)
+                .setTitle("Demo")
+                .setBackEnable(true)
+                .setImageArray(mImageArray, mColorArray)
+                .setupWithViewPager(mViewPager);
+    }
+
+    private void initFragments() {
+        mFragments = new ArrayList<>();
+        for (String title : mTitles) {
+            mFragments.add(MainFragment.getInstance(title));
+        }
+    }
+
+    private void initViewPager() {
+        mViewPager = (ViewPager) findViewById(R.id.vp);
+        mViewPager.setOffscreenPageLimit(4);
+        mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), mFragments, mTitles));
+    }
+
+}
